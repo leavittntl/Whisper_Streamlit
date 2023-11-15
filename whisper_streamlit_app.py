c724bbb795b11f3f0e4eb07918fce6269e110747
leavittntl <71446166+leavittntl@users.noreply.github.com>
2023-11-15 15:39:55 -0500

    Update main and req

--- whisper_streamlit_app.py
+++ whisper_streamlit_app.py
@@ -1,28 +1,32 @@
 import streamlit as st
 import whisper
+import soundfile as sf
 from io import BytesIO
 
-# Load Whisper model - consider doing this outside of the function to avoid reloading for each user
-model = whisper.load_model("base")
-
-@st.cache(allow_output_mutation=True)
+# Load Whisper model
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None, "builtins.weakref": lambda _: None})
 def load_model():
     return whisper.load_model("base")
 
 model = load_model()
 
 def transcribe_audio(audio_data):
-    audio_buffer = BytesIO(audio_data)
-    result = model.transcribe(audio_buffer)
+    # Convert BytesIO object to NumPy array expected by Whisper
+    with sf.SoundFile(audio_data) as audio:
+        samples = audio.read(dtype="float32")
+    result = model.transcribe(samples)
     return result["text"]
 
 st.title("Whisper Audio Transcription App")
 
-uploaded_file = st.file_uploader("Choose an audio file...")
+uploaded_file = st.file_uploader("Choose an audio file...", type=['mp3', 'wav', 'flac', 'ogg', 'mp4'])
 
 if uploaded_file is not None:
+    bytes_io = uploaded_file.getvalue()
+    buffer = BytesIO(bytes_io)
+    
     with st.spinner("Transcribing..."):
-        transcription = transcribe_audio(uploaded_file.getvalue())
+        transcription = transcribe_audio(buffer)
         
         # Display the results
         st.subheader("Transcription Result")
