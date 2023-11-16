993f08308b0a4f8c8d79250cdc82a515a7646023
leavittntl <71446166+leavittntl@users.noreply.github.com>
2023-11-16 09:06:25 -0500

    Update Main and Req

--- whisper_streamlit_app.py
+++ whisper_streamlit_app.py
@@ -1,35 +1,48 @@
 import streamlit as st
 import whisper
-import soundfile as sf
-from io import BytesIO
 
-# Load Whisper model
-@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None, "builtins.weakref": lambda _: None})
-def load_model():
-    return whisper.load_model("base")
+# Initialize the model to None and load it on demand
+model = None
 
-model = load_model()
+def load_model(model_size='tiny'):
+    global model
+    model = whisper.load_model(model_size)
 
-def transcribe_audio(audio_data):
-    # Convert BytesIO object to NumPy array expected by Whisper
-    with sf.SoundFile(audio_data) as audio:
-        samples = audio.read(dtype="float32")
-    result = model.transcribe(samples)
-    return result["text"]
+def transcribe_audio(audio_file):
+    global model
+    if not model:
+        load_model()  # Load the model if it hasn't been loaded already
+    return model.transcribe(audio_file)
 
-st.title("Whisper Audio Transcription App")
+def main():
+    st.title('Audio Transcription with OpenAI Whisper')
 
-uploaded_file = st.file_uploader("Choose an audio file...", type=['mp3', 'wav', 'flac', 'ogg', 'mp4'])
+    # Audio file uploader
+    audio_file = st.file_uploader("Upload an audio file", type=['mp3', 'wav', 'ogg', 'webm'])
 
-if uploaded_file is not None:
-    bytes_io = uploaded_file.getvalue()
-    buffer = BytesIO(bytes_io)
-    
-    with st.spinner("Transcribing..."):
-        transcription = transcribe_audio(buffer)
-        
-        # Display the results
-        st.subheader("Transcription Result")
-        st.write(transcription)
-else:
-    st.write("Upload an audio file to get started.")
+    if audio_file is not None:
+        # Display an audio player widget
+        st.audio(audio_file, format='audio/webm')  # Use the appropriate audio format
+
+        # Confirm before transcribing
+        if st.button('Transcribe Audio'):
+            try:
+                # Show a message while the transcription is being processed
+                with st.spinner('Transcribing...'):
+                    # Save the uploaded file to disk for Whisper processing
+                    audio_file_path = audio_file.name
+                    with open(audio_file_path, "wb") as f:
+                        f.write(audio_file.getvalue())
+                    
+                    # Transcribe the audio
+                    result = transcribe_audio(audio_file_path)
+                    
+                    # Display the transcription with timestamps
+                    for segment in result["segments"]:
+                        st.write(f"{segment['start']} - {segment['end']}: {segment['text']}")
+            except Exception as e:
+                # If an error occurs, display the error message
+                st.error(f'An error occurred during transcription: {e}')
+
+if __name__ == "__main__":
+    main()
